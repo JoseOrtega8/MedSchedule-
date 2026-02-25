c16e6cae34239878f4eb4619340fda3099d27aa3
Josor2 <jo1296914@gmail.com>
2026-02-24 20:11:33 -0700

    feat(migraciones): agregar Spatie permission y corregir migraciones
    Branch: feat/5-migraciones-bd
    Fixes: #5

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
 			$table->timestamp('email_verified_at')->nullable();
 			$table->string('password');
 			$table->string('phone', 20)->nullable();
-			$table->enum('role', ['admin', 'doctor', 'patient'])->default('patient');
 			$table->integer('status')->default(1); // 1 = active, 0 = inactive
 			$table->rememberToken();
 			$table->timestamps();
